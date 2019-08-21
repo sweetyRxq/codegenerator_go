5741f30a2e4abc334a2da78d34f77716addd1c6e
renxiaoqing <renxiaoqing@mingchaintech.com>
2019-08-21 16:39:39 +0800

    push at datetime

--- model/ResourceCar.go
+++ model/ResourceCar.go
@@ -2,10 +2,12 @@ package model
 
 type ResourceCar struct {
 //GENERATE_START	 
+	 
 	CarId string`json:"carId"`	 
 	CarNo string`json:"carNo"`	 
 	PlateNum string`json:"plateNum"`	 
 	State int`json:"state"`	
 	DataType string `json:"dataType"`
+
 //GENERATE_END
 }
